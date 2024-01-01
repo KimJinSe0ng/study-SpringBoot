483c16c0abed5d09b9c730f3a135c4f008dde987
KimJinSe0ng <waujoxc@gmail.com>
2024-01-01 20:33:38 +0900

    sec 3 # 02
    회원 도메인과 리포지토리 생성

---
+++ hello-spring/src/main/java/hello/hellospring/repository/MemberRepository.java
@@ -0,0 +1,17 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+
+    Optional<Member> findById(Long id);
+
+    Optional<Member> findByName(String name); //Optional은 findByName으로 가져오는데 null이 반환될 수 있는데, null을 그대로 반환하는 것보다 Optional로 감싸서 반환하는 방법 선호
+
+    List<Member> findAll();
+
+}
---
+++ hello-spring/src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -0,0 +1,36 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberRepository {
+
+    private static Map<Long, Member> store = new HashMap<>(); //공유되는 변수는 ConcurrentHashMap()사용할 것 (실무에서)
+    private static long sequence = 0L; //얘도 동시성 고려해서 Atomic을 해야 함(실무에서)
+
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(store.get(id)); //Optional.ofNullable()는 null을 감싸서 클라이언트에서 처리가능하게 할 수 있음
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name))
+                .findAny();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+}
