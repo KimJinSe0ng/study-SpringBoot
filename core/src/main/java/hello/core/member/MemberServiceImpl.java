5212846ba9f662124e92b95c5f773617f87986e8
KimJinSe0ng <waujoxc@gmail.com>
2024-01-02 23:34:25 +0900

    sec 2 # 05
    회원 도메인 실행과 테스트

--- core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,7 +2,7 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService{
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository = new MemoryMemberRepository(); //문제점: 추상화와 구현체에 둘 다 의존한다.
     @Override
     public void join(Member member) {
         memberRepository.save(member);
