28b5ef96443666797a3f894e126ded36faf6c702
KimJinSe0ng <waujoxc@gmail.com>
2024-01-03 00:08:24 +0900

    sec 3 # 02
    DIP 문제는 해결했으나 테스트 실패

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,8 +1,6 @@
 package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
-import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
@@ -10,8 +8,7 @@ import hello.core.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository = new MemoryMemberRepository(); //문제점: 추상화(인터페이스)뿐만 아니라 구현 클래스도 의존하고 있다.
-//    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
-    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+    private DiscountPolicy discountPolicy; //추상화에만 의존하도록 변경했다.
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
