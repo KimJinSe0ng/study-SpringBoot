82c74ddc9766442ab6777b929e3c04481e6af7ad
KimJinSe0ng <waujoxc@gmail.com>
2024-01-08 22:03:44 +0900

    sec 3 # 05
    새로운 구조와 할인 정책 적용

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -2,6 +2,7 @@ package hello.core;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -23,6 +24,7 @@ public class AppConfig { //구성정보
     }
 
     public DiscountPolicy discountPolicy() { //역할
-        return new FixDiscountPolicy(); //구현
+//        return new FixDiscountPolicy(); //구현
+        return new RateDiscountPolicy();
     }
 }
