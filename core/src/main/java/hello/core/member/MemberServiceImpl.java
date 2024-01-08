b8742391dc793e3f3fe22dab14658da7b60284a7
KimJinSe0ng <waujoxc@gmail.com>
2024-01-08 21:54:21 +0900

    sec 3 # 03
    AppConfig 리팩터링

--- core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,7 +2,12 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService{
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository(); //문제점: 추상화와 구현체에 둘 다 의존한다.
+    private final MemberRepository memberRepository; //문제점: 추상화와 구현체에 둘 다 의존한다.
+
+    public MemberServiceImpl(MemberRepository memberRepository) { //memberRepository의 구현체에 뭐가 들어갈지 생성자를 통해 주입해준다.
+        this.memberRepository = memberRepository;
+    }
+
     @Override
     public void join(Member member) {
         memberRepository.save(member);
--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -7,8 +7,13 @@ import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository(); //문제점: 추상화(인터페이스)뿐만 아니라 구현 클래스도 의존하고 있다.
-    private DiscountPolicy discountPolicy; //추상화에만 의존하도록 변경했다.
+    private final MemberRepository memberRepository; //문제점: 추상화(인터페이스)뿐만 아니라 구현 클래스도 의존하고 있다.
+    private final DiscountPolicy discountPolicy; //추상화에만 의존하도록 변경했다.
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
