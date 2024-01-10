afbf288c04c7ba3ac07c125befbbe460901db2fa
KimJinSe0ng <waujoxc@gmail.com>
2024-01-10 23:34:59 +0900

    sec 5 # 05
    @Configuration과 싱글톤

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -14,18 +14,35 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppConfig { //구성정보
 
+    //@Bean memberService -> new MemoryMemberRepository()
+    //@Bean orderService -> new MemoryMemberRepository() 이렇게 2번 호출이 되는데 싱글톤이 깨지는 것처럼 보인다. 깨질까? 안 깨질까?
+
+    //예측
+    //call AppConfig.memberService
+    //call AppConfig.memberRepository
+    //call AppConfig.memberRepository
+    //call AppConfig.orderService
+    //call AppConfig.memberRepository
+
+    //실제 : 스프링이 어떻게든 싱글톤으로 해준다.
+    //call AppConfig.memberService
+    //call AppConfig.memberRepository
+    //call AppConfig.orderService
     @Bean
     public MemberService memberService() {
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
     public MemoryMemberRepository memberRepository() { //역할
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository(); //구현
     }
 
     @Bean
     public OrderService orderService() { //역할
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy()); //구현
     }
 
