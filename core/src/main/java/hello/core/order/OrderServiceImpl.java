ac54fe4adcac336f9bbf0dc8f0b8315fea923553
KimJinSe0ng <waujoxc@gmail.com>
2024-01-14 16:46:55 +0900

    sec 7 # 02
    옵션 처리

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -10,11 +10,17 @@ import org.springframework.stereotype.Component;
 @Component
 public class OrderServiceImpl implements OrderService {
 
-    private final MemberRepository memberRepository; //문제점: 추상화(인터페이스)뿐만 아니라 구현 클래스도 의존하고 있다.
-    private final DiscountPolicy discountPolicy; //추상화에만 의존하도록 변경했다.
+    private MemberRepository memberRepository; //문제점: 추상화(인터페이스)뿐만 아니라 구현 클래스도 의존하고 있다.
+    private DiscountPolicy discountPolicy; //추상화에만 의존하도록 변경했다.
+
+//    @Autowired
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+    public void init(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
