ad75b9e0f0a4da6be965c135bc6f8edac02a87e1
KimJinSe0ng <waujoxc@gmail.com>
2024-01-14 14:19:56 +0900

    sec 6 # 03
    필터

---
+++ core/src/test/java/hello/core/scan/filter/MyIncludeComponent.java
@@ -0,0 +1,9 @@
+package hello.core.scan.filter;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.TYPE) //클래스 레벨(타입)에 붙느냐, 필드에 붙느냐 지정
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface MyIncludeComponent {
+}
