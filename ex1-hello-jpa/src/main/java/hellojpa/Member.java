0190d40157bbe57ae5f927e860a95c64ba8a339b
KimJinSe0ng <waujoxc@gmail.com>
2024-01-21 00:29:08 +0900

    sec 4 # 02
    데이터베이스 스키마 자동 생성

--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -1,18 +1,20 @@
 package hellojpa;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity //JPA가 로딩될 때 JPA가 관리해야겠다고 인식
-@Table(name = "USER") //데이터베이스 USER 테이블과 자동 매핑 : 기본값은 클래스명, 예를 들어 쿼리가 from USER 이렇게 바뀜
+//@Table(name = "USER") //데이터베이스 USER 테이블과 자동 매핑 : 기본값은 클래스명, 예를 들어 쿼리가 from USER 이렇게 바뀜
 public class Member {
 
     @Id
     private Long id;
-//    @Column(name = "username") // username 컬럼과 자동 매핑 : 기본값은 변수명
+//    @Column(name = "username") //데이터베이스 username 컬럼과 자동 매핑 : 기본값은 변수명
+    @Column(unique = true, length = 10)
     private String name;
-
+    private int age;
     public Member() { //JPA는 기본 생성자가 필수 - 동적으로 객체를 생성해야하는 경우가 있음
     }
 
