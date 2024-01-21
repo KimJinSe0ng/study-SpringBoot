a491033bff0500a59dabbb59a31db50193339b2f
KimJinSe0ng <waujoxc@gmail.com>
2024-01-22 00:28:38 +0900

    sec 6 # 02
    일대다 양방향

--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -26,6 +26,10 @@ public class Member {
     @Column(name = "USERNAME") //DB 컬럼명은 name 이야 하면 name으로 Insert 함
     private String username;
 
+    @ManyToOne
+    @JoinColumn(name = "TEAM_ID", insertable = false, updatable = false)
+    private Team team;
+
     public Long getId() {
         return id;
     }
