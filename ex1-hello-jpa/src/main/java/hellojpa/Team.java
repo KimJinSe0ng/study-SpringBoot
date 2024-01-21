ab6394fc976e78108c77159af4c83b60fa4a0dfc
KimJinSe0ng <waujoxc@gmail.com>
2024-01-22 00:12:47 +0900

    sec 6 # 01
    다대일 양방향

--- ex1-hello-jpa/src/main/java/hellojpa/Team.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Team.java
@@ -12,6 +12,8 @@ public class Team {
     @Column(name = "TEAM_ID")
     private Long id;
     private String name;
+    @OneToMany(mappedBy = "team") //mappedBy는 1:다 매핑에서 반대편의 뭐랑 연결되어 있는지? Member 객체의 team 변수명 -> 나는 team으로 매핑이 되어 있다는 뜻
+    private List<Member> members = new ArrayList<>();
 
     public Long getId() {
         return id;
