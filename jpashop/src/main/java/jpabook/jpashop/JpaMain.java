bc91a59102ea94343e0e652371447f50a62ca225
KimJinSe0ng <waujoxc@gmail.com>
2024-01-22 23:45:37 +0900

    sec 7 # 03
    상속관계 매핑

--- jpashop/src/main/java/jpabook/jpashop/JpaMain.java
+++ jpashop/src/main/java/jpabook/jpashop/JpaMain.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop;
 
 
+import jpabook.jpashop.domain.Book;
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderItem;
@@ -20,15 +21,10 @@ public class JpaMain {
 
         tx.begin();
         try {
-
-            Order order = new Order();
-//            order.addOrderItem(new OrderItem());
-            em.persist(order);
-
-            OrderItem orderItem = new OrderItem();
-            orderItem.setOrder(order);
-
-            em.persist(orderItem);
+            Book book = new Book();
+            book.setName("JPA");
+            book.setAuthor("김영한");
+            em.persist(book);
 
             tx.commit();
         } catch (Exception e) {
