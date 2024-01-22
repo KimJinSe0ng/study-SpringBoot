216c74188cea41d7b1df1f4a833abb25bc22db7d
KimJinSe0ng <waujoxc@gmail.com>
2024-01-22 22:30:38 +0900

    sec 6 # 03
    일대일

--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -3,22 +3,9 @@ package hellojpa;
 import javax.persistence.*;
 
 @Entity //JPA가 로딩될 때 JPA가 관리해야겠다고 인식
-//@Table(name = "USER") //데이터베이스 USER 테이블과 자동 매핑: 기본값은 클래스명, 예를 들어 쿼리가 from USER 이렇게 바뀜
-//@SequenceGenerator(name = "member_seq_generator", sequenceName = "member_seq")
-//@TableGenerator(
-//        name = "MEMBER_SEQ_GENERATOR",
-//        table = "MY_SEQUENCE",
-//        pkColumnValue = "MEMBER_SEQ", allocationSize = 1
-//)
-//@SequenceGenerator(
-//        name = "MEMBER_SEQ_GENERATOR",
-//        sequenceName = "MEMBER_SEQ", //매핑할 데이터베이스 시퀀스 이름
-//        initialValue = 1, allocationSize = 50)
 public class Member {
 
     @Id
-//    @GeneratedValue(strategy = GenerationType.TABLE, generator = "MEMBER_SEQ_GENERATOR")
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @GeneratedValue
     @Column(name = "MEMBER_ID")
     private Long id;
@@ -29,6 +16,10 @@ public class Member {
     @ManyToOne
     @JoinColumn(name = "TEAM_ID", insertable = false, updatable = false)
     private Team team;
+
+    @OneToOne
+    @JoinColumn(name = "LOCKER_ID")
+    private Locker locker;
 
     public Long getId() {
         return id;
