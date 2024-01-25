402b82c715b821e121a8807eaa70c3d97e6f6aa1
KimJinSe0ng <waujoxc@gmail.com>
2024-01-25 21:01:35 +0900

    sec 9 # 06
    값 타입 매핑

--- jpashop/src/main/java/jpabook/jpashop/domain/Delivery.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -10,10 +10,40 @@ public class Delivery extends BaseEntity {
     @Id @GeneratedValue
     private Long id;
     private String city;
-    private String street;
-    private String zipcode;
-    private DeliveryStatus status;
-
+    @Embedded
+    private Address address;
     @OneToOne(mappedBy = "delivery", fetch = LAZY)
     private Order order;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
 }
