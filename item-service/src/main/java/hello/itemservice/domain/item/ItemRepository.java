fdab7e272c1cbe188a2daaa4e272e62fa2aef366
KimJinSe0ng <waujoxc@gmail.com>
2024-02-02 16:32:21 +0900

    sec 7 # 03
    상품 도메인 개발

---
+++ item-service/src/main/java/hello/itemservice/domain/item/ItemRepository.java
@@ -0,0 +1,40 @@
+package hello.itemservice.domain.item;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository
+public class ItemRepository {
+
+    private static final Map<Long, Item> store = new HashMap<>(); //static 사용했다는 것 주의
+    private static long sequence = 0; //static
+
+    public Item save(Item item) {
+        item.setId(++sequence);
+        store.put(item.getId(), item);
+        return item;
+    }
+
+    public Item findById(Long id) {
+        return store.get(id);
+    }
+
+    public List<Item> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+    public void update(Long itemId, Item updateParam) { //itemId를 사용하지 않기 때문에 DTO를 만들어서 명확하게 나누는게 좋음
+        Item findItem = findById(itemId);
+        findItem.setItemName(updateParam.getItemName());
+        findItem.setPrice(updateParam.getPrice());
+        findItem.setQuantity(updateParam.getQuantity());
+    }
+
+    public void clearStore() {
+        store.clear();
+    }
+}
