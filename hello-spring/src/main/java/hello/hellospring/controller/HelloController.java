8328051c53ba30874b8be4dcfa8a011232eacdb6
KimJinSe0ng <waujoxc@gmail.com>
2024-02-03 23:34:31 +0900

    refactor(controller)

--- hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -20,13 +20,13 @@ public class HelloController {
         return "hello-template";
     }
 
-    @GetMapping("hello-string")
+    @GetMapping("hello-string") //@ResponseBody 문자 반환
     @ResponseBody //http는 헤더부, 바디부가 있는데 바디부에 이 데이터 내용을 직접 넣어주겠다.
     public String helloString(@RequestParam("name") String name) {
         return "hello " + name; //"hello String"
     }
 
-    @GetMapping("hello-api")
+    @GetMapping("hello-api") //@ResponseBody 객체 반환
     @ResponseBody//api 방식 : Json
     //ResponseBody가 있으면 HttpMessageConverter가 동작한다. 단순 문자면 StringConverter가 작동, 객체면 JsonConverter가 작동
     public Hello helloApi(@RequestParam("name") String name) {
