8a6fae9a3041a5b8db0602008d740a578577f1d6
KimJinSe0ng <waujoxc@gmail.com>
2024-02-06 11:53:25 +0900

    refactor(servletmvc)

--- servlet/src/main/java/hello/servlet/web/servletmvc/MvcMemberSaveServlet.java
+++ servlet/src/main/java/hello/servlet/web/servletmvc/MvcMemberSaveServlet.java
@@ -26,11 +26,11 @@ public class MvcMemberSaveServlet extends HttpServlet {
         memberRepository.save(member); //비즈니스 로직 호출
 
         //Model에 데이터를 보관한다.
-        request.setAttribute("member",member); //request 객체 내부 저장소가 있는데(map같은 것) 멤버가 저장됨
+        request.setAttribute("member", member); //request 객체 내부 저장소가 있는데(map같은 것) 멤버가 저장됨
 
         //뷰로 던짐
         String viewPath = "/WEB-INF/views/save-result.jsp";
         RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
-        dispatcher.forward(request, response);
+        dispatcher.forward(request, response); //request 객체(Model)에 member가 저장되어 있고, 이를 뷰로 전달
     }
 }
