de8d47808c07b89fddaa46576e33ea474e9b5ac9
KimJinSe0ng <waujoxc@gmail.com>
2024-02-07 00:41:26 +0900

    sec 7 # 09
    주문 목록 검색, 취소

--- jpashop2/src/main/java/jpabook/jpashop2/controller/OrderController.java
+++ jpashop2/src/main/java/jpabook/jpashop2/controller/OrderController.java
@@ -1,16 +1,16 @@
 package jpabook.jpashop2.controller;
 
 import jpabook.jpashop2.domain.Member;
+import jpabook.jpashop2.domain.Order;
 import jpabook.jpashop2.item.Item;
+import jpabook.jpashop2.repository.OrderSearch;
 import jpabook.jpashop2.service.ItemService;
 import jpabook.jpashop2.service.MemberService;
 import jpabook.jpashop2.service.OrderService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -41,6 +41,20 @@ public class OrderController {
 
         orderService.order(memberId, itemId, count); //command성, 주문 등은 외부에서 컨트롤러 레벨에서는 식별자만 넘기고, 실제 핵심 비즈니스 서비스에서 엔티티를 찾는 것 부터 하게 개발함
         //핵심 비즈니스 로직을 트랜잭션 안에서 하게 되면 영속성 컨텍스트가 있는 상태에서 조회할 수 있어서 더티 체킹하며 값을 바꿔줘도 자연스럽게 적용이 됨
+        return "redirect:/orders";
+    }
+
+    @GetMapping("/orders")
+    public String orderList(@ModelAttribute("orderSearch") OrderSearch orderSearch, Model model) { //화면 그려야 하니까 model 넘기고
+        List<Order> orders = orderService.findOrders(orderSearch);
+        model.addAttribute("orders", orders);
+
+        return "order/orderList";
+    }
+
+    @PostMapping("/orders/{orderId}/cancel")
+    public String cancelOrder(@PathVariable("orderId") Long orderId) {
+        orderService.cancelOrder(orderId);
         return "redirect:/orders";
     }
 }
