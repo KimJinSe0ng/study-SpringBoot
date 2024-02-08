b7ac8acded343e2b59ac845fcd657c2c8350bb25
KimJinSe0ng <waujoxc@gmail.com>
2024-02-08 20:22:21 +0900

    sec 4 # 01
    엔티티 직접 노출

---
+++ jpashop2/src/main/java/jpabook/jpashop2/api/OrderApiController.java
@@ -0,0 +1,35 @@
+package jpabook.jpashop2.api;
+
+import jpabook.jpashop2.domain.Order;
+import jpabook.jpashop2.domain.OrderItem;
+import jpabook.jpashop2.repository.OrderRepository;
+import jpabook.jpashop2.repository.OrderSearch;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderApiController {
+
+    private final OrderRepository orderRepository;
+
+    /**
+     * V1. 엔티티 직접 노출
+     * - Hibernate5Module 모듈 등록, LAZY=null 처리 * - 양방향 관계 문제 발생 -> @JsonIgnore
+     */
+    @GetMapping("/api/v1/orders")
+    public List<Order> ordersV1() {
+        List<Order> all = orderRepository.findAllByString(new OrderSearch());
+        for (Order order : all) {
+            order.getMember().getName();
+            order.getDelivery().getAddress();
+
+            List<OrderItem> orderItems = order.getOrderItems(); //강제 초기화
+            orderItems.stream().forEach(o -> o.getItem().getName()); //주문과 관련된 orderItems를 다 가져와서 초기화
+        }
+        return all;
+    }
+}
