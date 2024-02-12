e3cf2eefb1aea3378cedbefe3f4bedd18f79fe64
KimJinSe0ng <waujoxc@gmail.com>
2024-02-12 14:23:32 +0900

    sec 7 # 04
    네이티브 쿼리

--- data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
+++ data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
@@ -18,16 +18,18 @@ public interface MemberRepository extends JpaRepository<Member, Long>, MemberRep
 
     List<Member> findByUsernameAndAgeGreaterThan(String username, int age); //도메인 특화된 조회
 
-//    @Query(name = "Member.findByUsername") //없어도 잘 동작함 -> <Member, Long>의 Member 엔티티에 점을 찍고 findByUsername 메서드 명으로 네임드 쿼리를 찾아서 실행하고, 없으면 메서드 쿼리로 이름 생성하는 방식으로 함
+    //    @Query(name = "Member.findByUsername") //없어도 잘 동작함 -> <Member, Long>의 Member 엔티티에 점을 찍고 findByUsername 메서드 명으로 네임드 쿼리를 찾아서 실행하고, 없으면 메서드 쿼리로 이름 생성하는 방식으로 함
     List<Member> findByUsername(@Param("username") String username);
 
-    @Query("select m from Member m where m.username = :username and m.age = :age") //이름이 없는 네임드 쿼리이기 때문에 실행 시점에 SQL 파싱하면서 에러를 잡아 줌
+    @Query("select m from Member m where m.username = :username and m.age = :age")
+        //이름이 없는 네임드 쿼리이기 때문에 실행 시점에 SQL 파싱하면서 에러를 잡아 줌
     List<Member> findUser(@Param("username") String username, @Param("age") int age);
 
     @Query("select m.username from Member m")
     List<String> findUsernameList();
 
-    @Query("select new study.datajpa.dto.MemberDto(m.id, m.username, t.name) from Member m join m.team t") //DTO는 new로 해서 해야 함
+    @Query("select new study.datajpa.dto.MemberDto(m.id, m.username, t.name) from Member m join m.team t")
+        //DTO는 new로 해서 해야 함
     List<MemberDto> findMemberDto();
 
     @Query("select m from Member m where m.username in :names")
@@ -43,10 +45,12 @@ public interface MemberRepository extends JpaRepository<Member, Long>, MemberRep
 //    Slice<Member> findByAge(int age, Pageable pageable); //Pageable 현재 내가 1페이지, 2페이지...
 
     @Query(value = "select m from Member m left join m.team t",
-            countQuery = "select count(m) from Member m") //쿼리가 복잡하면 성능 테스트 후 카운트 쿼리 분리해야 함
+            countQuery = "select count(m) from Member m")
+        //쿼리가 복잡하면 성능 테스트 후 카운트 쿼리 분리해야 함
     Page<Member> findByAge(int age, Pageable pageable); //Pageable 현재 내가 1페이지, 2페이지...
 
-    @Modifying(clearAutomatically = true) //@Modifying가 있어야 executeUpdate 해줌, clearAutomatically = true는 쿼리가 나가고 em.clear()기능 자동으로 해줌
+    @Modifying(clearAutomatically = true)
+    //@Modifying가 있어야 executeUpdate 해줌, clearAutomatically = true는 쿼리가 나가고 em.clear()기능 자동으로 해줌
     @Query("update Member m set m.age = m.age + 1 where m.age >= :age")
     int bulkAgePlus(@Param("age") int age);
 
@@ -54,7 +58,8 @@ public interface MemberRepository extends JpaRepository<Member, Long>, MemberRep
     List<Member> findMemberFetchJoin();
 
     @Override
-    @EntityGraph(attributePaths = {"team"}) //JPQL 말고, member도 조회하며 team도 조회하고 싶어 -> EntityGraph는 내부적으로 페치 조인 실행
+    @EntityGraph(attributePaths = {"team"})
+        //JPQL 말고, member도 조회하며 team도 조회하고 싶어 -> EntityGraph는 내부적으로 페치 조인 실행
     List<Member> findAll();
 
     @EntityGraph(attributePaths = {"team"})
@@ -72,7 +77,17 @@ public interface MemberRepository extends JpaRepository<Member, Long>, MemberRep
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     List<Member> findLockByUsername(String username);
 
-//    List<UsernameOnly> findProjectionsByUsername(String username); //인터페이스 기반
-//    List<UsernameOnlyDto> findProjectionsByUsername(String username); //클래스 기반 Projections
-    <T> List<T> findProjectionsByUsername(String username, Class<T> type); //동적 Projections
+    //    List<UsernameOnly> findProjectionsByUsername(String username); //인터페이스 기반
+    //    List<UsernameOnlyDto> findProjectionsByUsername(String username); //클래스 기반 Projections
+    <T> List<T> findProjectionsByUsername(@Param("username") String username, Class<T> type); //동적 Projections
+
+    @Query(value = "select * from member where username = ?", nativeQuery = true)
+    Member findByNativeQuery(String username);
+
+    //DTO를 뽑는데 편하게 뽑을 수 있고 네이티브 쿼리고 동적 쿼리는 아니야 이럴 때 씀
+    @Query(value = "SELECT m.member_id as id, m.username, t.name as teamName " +
+            "FROM member m left join team t ON m.team_id = t.team_id",
+            countQuery = "SELECT count(*) from member",
+            nativeQuery = true)
+    Page<MemberProjection> findByNativeProjection(Pageable pageable);
 }
