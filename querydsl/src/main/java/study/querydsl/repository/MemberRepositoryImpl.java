f4bbee82b905702b2bf12c7717c2860e9b91899f
KimJinSe0ng <waujoxc@gmail.com>
2024-02-13 11:37:16 +0900

    sec 6 # 03
    페이징 활용 - Querydsl 페이징 연동

--- querydsl/src/main/java/study/querydsl/repository/MemberRepositoryImpl.java
+++ querydsl/src/main/java/study/querydsl/repository/MemberRepositoryImpl.java
@@ -1,8 +1,12 @@
 package study.querydsl.repository;
 
+import com.querydsl.core.QueryResults;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.dto.QMemberTeamDto;
@@ -40,6 +44,77 @@ public class MemberRepositoryImpl implements MemberRepositoryCustom {
                         ageLoe(condition.getAgeLoe())
                 )
                 .fetch();
+    }
+
+    @Override
+    public Page<MemberTeamDto> searchPageSimple(MemberSearchCondition condition, Pageable pageable) {
+        QueryResults<MemberTeamDto> results = queryFactory
+                .select(new QMemberTeamDto(
+                        member.id.as("memberId"),
+                        member.username,
+                        member.age,
+                        team.id.as("teamId"),
+                        team.name.as("teamName")))
+                .from(member)
+                .leftJoin(member.team, team)
+                .where(
+                        usernameEq(condition.getUsername()),
+                        teamNameEq(condition.getTeamName()),
+                        ageGoe(condition.getAgeGoe()),
+                        ageLoe(condition.getAgeLoe())
+                )
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetchResults(); //fetch() 쓰면 반환타입이 데이터 컨텐츠를 바로 가져옴 List<MemberTeamDto>
+                //fetchResults() 쓰면 컨텐츠용 쿼리도 날리고, 카운트용 쿼리도 날려서 2번 나림
+
+        List<MemberTeamDto> content = results.getResults();
+        long total = results.getTotal();
+
+        return new PageImpl<>(content, pageable, total);
+    }
+
+    @Override
+    public Page<MemberTeamDto> searchPageComplex(MemberSearchCondition condition, Pageable pageable) {
+        List<MemberTeamDto> content = queryFactory
+                .select(new QMemberTeamDto(
+                        member.id.as("memberId"),
+                        member.username,
+                        member.age,
+                        team.id.as("teamId"),
+                        team.name.as("teamName")))
+                .from(member)
+                .leftJoin(member.team, team)
+                .where(
+                        usernameEq(condition.getUsername()),
+                        teamNameEq(condition.getTeamName()),
+                        ageGoe(condition.getAgeGoe()),
+                        ageLoe(condition.getAgeLoe())
+                )
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch(); //fetch() 쓰면 반환타입이 데이터 컨텐츠만 바로 가져옴 List<MemberTeamDto>
+                //fetchResults() 쓰면 컨텐츠용 쿼리도 날리고, 카운트용 쿼리도 날려서 2번 나림
+
+        //첫 번째는 알아서 카운트 쿼리 날리고, 두 번째는 내가 직접 카운트 쿼리 날림
+        //토탈 카운트용 쿼리를 따로 만드는 것이 차이점
+        long total = queryFactory
+                .select(member)
+                .from(member)
+                .leftJoin(member.team, team) //어떤 상황에는 조인이 필요 없을 때도 있고, 컨텐츠 쿼리는 복잡한테 카운트 쿼리는 심플하게 만들 수 있을 때
+                //fetchResults()를 쓰면 join, where 다 붙어서 최적화가 불가능, 반대로 카운트 쿼리 먼저 후 조회되는게 없으면 컨텐츠 쿼리를 하지 않는 식으로 설계도 가능
+                .where(
+                        usernameEq(condition.getUsername()),
+                        teamNameEq(condition.getTeamName()),
+                        ageGoe(condition.getAgeGoe()),
+                        ageLoe(condition.getAgeLoe())
+                )
+                .fetchCount();
+
+//        List<MemberTeamDto> content = results.getResults();
+//        long total = results.getTotal();
+
+        return new PageImpl<>(content, pageable, total);
     }
 
     private BooleanExpression ageBetween(int ageLoe, int ageGoe) {
