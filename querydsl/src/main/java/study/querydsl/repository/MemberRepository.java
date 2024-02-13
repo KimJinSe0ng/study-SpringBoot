96c544ed7659079790c244e6e74d58b3b5e48be8
KimJinSe0ng <waujoxc@gmail.com>
2024-02-13 12:04:00 +0900

    sec 7 # 01
    인터페이스 지원

--- querydsl/src/main/java/study/querydsl/repository/MemberRepository.java
+++ querydsl/src/main/java/study/querydsl/repository/MemberRepository.java
@@ -1,11 +1,12 @@
 package study.querydsl.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import study.querydsl.entity.Member;
 
 import java.util.List;
 
-public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom { //MemberRepositoryCustom 를 상속받기 때문에 search()를 외부에서 호출 가능
+public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom, QuerydslPredicateExecutor { //MemberRepositoryCustom 를 상속받기 때문에 search()를 외부에서 호출 가능
 
     //select m from Member m where m.username = ?
     List<Member> findByUsername(String username);
